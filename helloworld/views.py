07a1f912c07b8d20e265585e4fedbabc21fbf9f2
no1 <eshijaku@umich.edu>
2018-11-29 23:05:54 -0500

    oauth implemented on load

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,14 +1,18 @@
 # helloworld/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from yahoo_oauth import OAuth2
 
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
+        oauth = OAuth2(None, None, from_file='files/oauth2.json')
+        print(oauth)
+
         return render(request, 'index.html', context=None)
 
 
 class GetLeague(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        return render(request, 'start.html', context=None)
